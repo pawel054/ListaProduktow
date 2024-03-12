c87d6a3c37fd86d943f321cf47887d7b56e3fe33
pawel054 <pawel.marcisz1@gmail.com>
2024-03-12 13:31:25 +0100

    Add Product Class

---
+++ ListaProduktow/ListaProduktow/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaProduktow
+{
+    public class Product
+    {
+        public string Name {  get; set; }
+        public decimal Price {  get; set; }
+        public int Count { get; set; }
+
+        public Product() { }
+        public Product(string name, decimal price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+    }
+}
