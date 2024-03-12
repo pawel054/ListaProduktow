ba707c537c2d5e3ebe164db24779910c67482064
pawel054 <pawel.marcisz1@gmail.com>
2024-03-12 13:34:23 +0100

    Add buttons handling

--- ListaProduktow/ListaProduktow/MainPage.xaml.cs
+++ ListaProduktow/ListaProduktow/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,29 @@ namespace ListaProduktow
 {
     public partial class MainPage : ContentPage
     {
+        private Product wybranyProdukt;
+        public ObservableCollection<Product> produkty = new ObservableCollection<Product>();
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public void Dodaj_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ManageProduct(produkty));
+        }
+        public void Edytuj_Clicked(object sender, EventArgs e)
+        {
+            if (lista.SelectedItem != null)
+            {
+                wybranyProdukt = (Product)lista.SelectedItem;
+                Navigation.PushAsync(new ManageProduct(wybranyProdukt));
+                wybranyProdukt = null;
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Nie wybrano elementu", "OK");
+            }
         }
     }
 }
--- ListaProduktow/ListaProduktow/ManageProduct.xaml.cs
+++ ListaProduktow/ListaProduktow/ManageProduct.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,24 @@ namespace ListaProduktow
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ManageProduct : ContentPage
     {
-        public ManageProduct()
+        private Product wybranyProdukt;
+        private ObservableCollection<Product> produkty = new ObservableCollection<Product>();
+        public ManageProduct(ObservableCollection<Product> produkty)
         {
             InitializeComponent();
+            this.produkty = produkty;
+        }
+
+        public ManageProduct(Product wybranyProdukt)
+        {
+            InitializeComponent();
+            this.wybranyProdukt = wybranyProdukt;
+            labelTytul.Text = "Edytuj produkt";
+            entryNazwa.Text = wybranyProdukt.Name;
+            entryCena.Text = wybranyProdukt.Price.ToString();
+            entryIlosc.Text = wybranyProdukt.Count.ToString();
+            btnDodaj.IsVisible = false;
+            btnEdytuj.IsVisible = true;
         }
     }
 }
